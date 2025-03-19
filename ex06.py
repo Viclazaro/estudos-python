4b44ec886729306e933b84f703e6254b392efea8
Yen <thearyazitah@gmail.com>
2025-03-19 09:25:00 -0300

    exercicios

--- ex06.py
+++ ex06.py
@@ -1,8 +1,8 @@
 # Crie duas variáveis que guarde dois números inteiros, e imprimar o quociente e o resto da divisão
 # inteira entre eles
 
-n1 = int(input('Digite um numero'))
-n2 =  int(input('Digite outro numero'))
+n1 = int(input('Digite um numero\n'))
+n2 =  int(input('Digite outro numero\n'))
 
 quociente = n1 // n2
 resto = n1 % n2
