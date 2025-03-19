df88e08726e2e89b60368139ee455efcb41e0687
Yen <thearyazitah@gmail.com>
2025-03-19 09:32:46 -0300

    exercicios

---
+++ ex07.py
@@ -0,0 +1,9 @@
+#Crie um algoritmo que irá guardar em uma variável um número inteiro. O algoritmo deve imprimir o 
+# antecessor e sucessor desse número
+
+num = 2
+result = num - 1
+result2 = num + 1
+
+print(f'Antecessor = {result}')
+print(f'Sucessor = {result2}')
