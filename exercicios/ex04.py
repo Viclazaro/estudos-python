014f0d6ffad0be678a628c2944ffc7b2df4483d2
Yen <thearyazitah@gmail.com>
2025-03-21 09:19:10 -0300

    exercicios

---
+++ exercicios/ex04.py
@@ -0,0 +1,15 @@
+#Crie uma variável que receba um valor que é pago por um produto, um segundo valor que é o preço do 
+#produto e retorne em outra variável o troco a ser dado. Considere que o valor pago será igual ou maior 
+#que o valor do produto.
+
+valor1 = float(input('Quanto você pagou?\n'))
+valor2 = float(input('Preço do produto:\n'))
+
+troco = valor1 - valor2
+
+print(f'O troco será = {troco}')
+
+
+
+
+
