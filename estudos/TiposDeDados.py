1a146aff7e36bd263dcde4ea924f45a149b9ef00
Yen <thearyazitah@gmail.com>
2025-03-22 20:03:53 -0300

    anotações

---
+++ estudos/TiposDeDados.py
@@ -0,0 +1,6 @@
+nome = "Victoria"  # String (texto)
+idade = 19       # Inteiro (int)
+altura = 1.71    # Float (número decimal)
+tem_pet = True   # Booleano (True ou False)
+
+print(nome, idade, altura, tem_pet)
--- estudos/variaveis.py
+++
@@ -1,5 +0,0 @@
-nome = "victoria" #variaveis são objetos, "=" em python recebe
-idade = 19
-peso = 50
-
-print(nome,idade,peso)
