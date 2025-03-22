86241742775c0e56899e0a4d43226821930a3708
Yen <thearyazitah@gmail.com>
2025-03-22 20:33:49 -0300

    exercicios

--- exercicios/ex08.py
+++ exercicios/ex08.py
@@ -2,3 +2,13 @@
 #do litro da gasolina e o valor que o motorista possui para colocar a gasolina. Imprima quantos litros de 
 #gasolina o motorista conseguiu colocar no tanque.
 
+#declarando variaveis
+valor_litro = float(input('Digite o valor da gasolina:\n'))
+valor_motorista = float(input('Digite o valor disponível\n'))
+
+#calculando quantidade de litros
+litros = valor_motorista / valor_litro
+
+#exibindo resultado
+print(f'Poderá colocar {litros:.2}L no tanque.')
+
