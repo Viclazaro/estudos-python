76a55dd3308a69dbbea761d6a34ccbd53d3e563c
Yen <thearyazitah@gmail.com>
2025-03-24 11:03:57 -0300

    add notes

--- concepts/input_data.py
+++ concepts/input_data.py
@@ -6,3 +6,11 @@ idade = input("Qual é a sua idade? ")  # Também retorna uma string, mesmo que 
 
 # Exibe o nome e a idade informados pelo usuário
 print(nome, idade)  # O print() exibe os valores separados por um espaço
+
+#estudos 2
+
+input() # utilizado para entrada de dados
+input("mensagem") # input reconhece dados como str
+
+x = int(input("Digite um numero: ")) # caso eu queria receber um numero é necessario declarar o tipo de variavel
+x = float(input("Digite um numero: ")) # outro exemplo
