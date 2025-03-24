57d7d776af7027fa799baa9611a468416045214e
Yen <thearyazitah@gmail.com>
2025-03-24 12:23:26 -0300

    exercises

---
+++ elif_exercises/ex_01.py
@@ -0,0 +1,11 @@
+# Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
+
+dado = input('Digite um caractere:\n')
+
+if dado == 'a' or dado == 'e' or dado == 'i' or dado == 'o' or dado == 'u' \
+or dado == 'A' or dado == 'E' or dado == 'I' or dado == 'O' or dado == 'U':
+
+    print('É uma vogal')
+
+else: 
+    print('É consoante')
---
+++ elif_exercises/ex_02.py
@@ -0,0 +1,18 @@
+#Faça um programa que pede duas notas de um aluno. Em seguida ele deve calcular a média do aluno e dar o seguinte resultado:
+#A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
+#   A mensagem "Reprovado", se a média for menor do que sete;
+#   A mensagem "Aprovado com Distinção", se a média for igual a dez.
+
+nota1 = float(input('Digite sua primeira nota'))
+nota2 = float(input('Digite sua segunda nota'))
+
+media = (nota1 + nota2)/2
+
+if media == 10:
+    print('Aprovado com distinção')
+
+elif media >= 7:
+    print('Aprovado')
+
+else:
+    print('Reprovado')
