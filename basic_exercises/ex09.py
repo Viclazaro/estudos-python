52b783ddef62840bd6ece98d28805eb943ef094d
Yen <thearyazitah@gmail.com>
2025-03-24 11:19:05 -0300

    exercises

--- basic_exercises/ex09.py
+++ basic_exercises/ex09.py
@@ -1,1 +1,10 @@
-# 9) 
+# 9) Crie um algoritmo que guarde em variáveis o nome de uma pessoa e o ano de nascimento dela. O 
+# algoritmo deve retornar o nome da pessoa e a sua idade.
+
+nome = input('Digite seu nome:\n') # Receber dado e armazenar na variavel "nome"
+data = int(input('Digite sua data de nascimento:\n')) # Receber dado e armazenar na variavel "data" declarando seu tipo
+
+idade = 2025 - data
+
+print(f'Seu nome é {nome}, possui {idade} anos de idade')
+
