1cc7970ea6ecef0c4f805843b4cc6ebceb1ad2b6
Yen <thearyazitah@gmail.com>
2025-03-24 11:07:24 -0300

    add notes

--- concepts/input_data.py
+++ concepts/input_data.py
@@ -17,18 +17,23 @@ x = float(input("Digite um numero: ")) # outro exemplo
 
 # exemplo 1
 
-salario1: float; salario2: float
-nome1: str; nome2: str
-idade: int
-sexo: str
+salario1: float; salario2: float  # Define que salario1 e salario2 são do tipo float
+nome1: str; nome2: str  # Define que nome1 e nome2 são do tipo string
+idade: int  # Define que idade é do tipo inteiro
+sexo: str  # Define que sexo é do tipo string
+
+# Coleta as entradas do usuário
 nome1 = input("Nome da primeira pessoa: ")
 salario1 = float(input("Salario da primeira pessoa: "))
 nome2 = input("Nome da segunda pessoa: ")
 salario2 = float(input("Salario da segunda pessoa: "))
 idade = int(input("Digite uma idade: "))
 sexo = input("Digite um sexo (F/M): ")
+
+# Imprime as informações coletadas com formatação
 print(f"Nome 1: {nome1}")
 print(f"Salario 1: {salario1:.2f}")
 print(f"Nome 2: {nome2}")
 print(f"Salario 2: {salario2:.2f}")
 print(f"Idade: {idade}")
+
