2d00df84b0c5b7e891021ed1f30111884af98c66
Yen <thearyazitah@gmail.com>
2025-06-04 09:01:53 -0300

    update

--- conditionals_exercises/ex_02.py
+++ conditionals_exercises/ex_02.py
@@ -1,7 +1,7 @@
 #Faça um programa que pede duas notas de um aluno. Em seguida ele deve calcular a média do aluno e dar o seguinte resultado:
 #A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
-#   A mensagem "Reprovado", se a média for menor do que sete;
-#   A mensagem "Aprovado com Distinção", se a média for igual a dez.
+#A mensagem "Reprovado", se a média for menor do que sete;
+#A mensagem "Aprovado com Distinção", se a média for igual a dez.
 
 nota1 = float(input('Digite sua primeira nota'))
 nota2 = float(input('Digite sua segunda nota'))
--- loops_exercises/ex_10.py
+++ loops_exercises/ex_10.py
@@ -1,5 +1,5 @@
 #Desenvolva um programa que faça a tabuada de um número qualquer inteiro que será digitado pelo usuário, mas a tabuada não deve necessariamente iniciar 
-# em 1 e terminar em 10, o valor inicial e final devem ser informados também pelo usuário, conforme exemplo abaixo:
+#em 1 e terminar em 10, o valor inicial e final devem ser informados também pelo usuário, conforme exemplo abaixo:
 #Montar a tabuada de: 5
 #Começar por: 4
 #Terminar em: 7
